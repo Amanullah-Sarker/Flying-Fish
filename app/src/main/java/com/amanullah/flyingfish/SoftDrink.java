7b036a1397c6d8e2085f95be109f181e22e2a466
Aminur <amanullahsarker3@gmail.com>
2021-10-04 14:57:09 +0600

    update

---
+++ app/src/main/java/com/amanullah/flyingfish/SoftDrink.java
@@ -0,0 +1,22 @@
+package com.amanullah.flyingfish;
+
+public class SoftDrink {
+
+    private String softDrink;
+
+    public SoftDrink(){
+
+    }
+
+    public SoftDrink(String softDrink) {
+        this.softDrink = softDrink;
+    }
+
+    public String getSoftDrink() {
+        return softDrink;
+    }
+
+    public void setSoftDrink(String softDrink) {
+        this.softDrink = softDrink;
+    }
+}
